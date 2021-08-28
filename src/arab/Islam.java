43b270b0150a28d20ad262d92bf10e5b69c78552
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-28 21:58:59 +0300

    Update and rename src/mindurka/Template.java to src/arab/Islam.java

---
+++ src/arab/Islam.java
@@ -0,0 +1,21 @@
+package mindurka;
+
+import arc.util.*;
+
+import mindustry.mod.*;
+import mindustry.ui.*;
+import mindustry.game.EventType.*;
+
+public class Islam extends Plugin {
+
+    @Override
+    public void init() {
+        Events.on(PlayerJoin.class, event -> {
+            String[][] options = {
+                {"[lime]Принять"},
+                {"[lime]Принять"}
+            };
+            Call.menu(event.player.con, 1, "[accent]ИСЛАМ, "[accent]Сделай свой выбор...", options);
+        });
+    }
+}
